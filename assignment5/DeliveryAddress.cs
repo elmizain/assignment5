d16aa11d65d84233c744dc4dfaec79863188e459
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-31 03:46:53 +0300

    question 1

---
+++ assignment5/DeliveryAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace assignment5
+{
+    public struct DeliveryAddress
+    {
+        public string City;
+        public string Street;
+        public int BuildingNumber;
+
+        public DeliveryAddress(string city, string street, int buildingNumber)
+        {
+            City = city;
+            Street = street;
+            BuildingNumber = buildingNumber;
+        }
+
+        public string GetFullAddress()
+        {
+            return $"City: {City}, Street: {Street}, Building Number: {BuildingNumber}";
+        }
+    }
+}
--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeliveryAddress address1 = new DeliveryAddress("Cairo", "Tahrir", 10);
+
+            DeliveryAddress address2 = address1;
+
+            address2.City = "Alexandria";
+
+            Console.WriteLine("Address 1:");
+            Console.WriteLine(address1.GetFullAddress());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Address 2:");
+            Console.WriteLine(address2.GetFullAddress());
+        }
+    }
+}
